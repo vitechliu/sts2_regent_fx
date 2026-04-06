438f637d5d06e151e2ff797d9e424082864c8400
Vitech <i@vitechliu.com>
2026-04-07 00:03:52 +0800

    Initial commit: RegentFX Godot VFX项目

---
+++ Scripts/Entry.cs
@@ -0,0 +1,19 @@
+using Godot.Bridge;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Modding;
+
+namespace Test.Scripts;
+
+// 必须要加的属性，用于注册Mod。字符串和初始化函数命名一致。
+[ModInitializer("Init")]
+public class Entry {
+    // 初始化函数
+    public static void Init() {
+        var harmony = new Harmony("sts2.vitech.regentFx");
+        harmony.PatchAll();
+        // 使得tscn可以加载自定义脚本
+        ScriptManagerBridge.LookupScriptsInAssembly(typeof(Entry).Assembly);
+        Log.Debug("Regent Fx Mod initialized!");
+    }
+}
---
+++ publish.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet publish RegentFX.csproj -c ExportRelease -f net9.0 -o ./bin/ExportRelease/net9.0/publish
