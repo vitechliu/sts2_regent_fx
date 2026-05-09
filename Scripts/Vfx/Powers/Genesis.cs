63b9632e3b4a80bd103d9584655a170131d6c92c
Vitech <i@vitechliu.com>
2026-05-09 19:21:33 +0800

    genesis

---
+++ Scripts/Vfx/Powers/Genesis.cs
@@ -0,0 +1,37 @@
+﻿using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Context;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Nodes;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Nodes.Vfx.Utilities;
+
+#pragma warning disable CS4014
+
+namespace RegentFX.Scripts.Vfx.Powers;
+
+[PowerFx(typeof(GenesisPower))]
+[HarmonyPatch]
+public class Genesis : PowerFX {
+    public override string? VfxScenePath => "res://RegentFX/scenes/vfx/genesis.tscn";
+    
+    public override void BeforeBeforeApplied(Creature target, Decimal amount) {
+        NCreature n = NCombatRoom.Instance?.GetCreatureNode(target);
+        if (n != null) {
+            TaskHelper.RunSafely(PlayAnim(n.VfxSpawnPosition));
+        }
+    }
+
+    async Task PlayAnim(Vector2 pos) {
+        VFXUtil.PlaySimple(VfxScenePath,pos, 4f);
+        await Cmd.Wait(0.5f);
+        WorldEnvironmentUtil.TweenExposure(2.5f, 0.25f);
+        await Cmd.Wait(0.25f);
+        Node2D ntest = VFXUtil.PlaySimple("res://RegentFX/scenes/vfx/distortions/vfx_outward_screen_distortion_ellipse.tscn", pos);
+        WorldEnvironmentUtil.TweenExposure(1f, 0.3f);
+    }
+}
